9da7864cf54f6c244604475d5f7fa8e8687abacd
Rodrigo Guzman <rodguze@gmail.com>
2016-10-20 10:10:08 -0400

    first working solution for paint problem

--- paint_your_solution.py
+++ paint_your_solution.py
@@ -6,8 +6,43 @@ def paint_splash( pixels, click_point, target_color ):
     # pixels: [ [ int ] ]
     # click_point: ( int, int )
     # target_color: int
-
-    # YOUR CODE HERE. Modify pixels in place. Or create a new array, I don't care.
-    # In place is better practice though.
+    x, y = click_point
+    click_color = pixels[x][y]
+    print('Starting! click point:', click_point, 'target color:', target_color)
+    splash_recurse(pixels, click_point, click_color, target_color)
 
     return pixels
+
+
+
+def splash_recurse(pixels, current_point, click_color, target_color, seen=None):
+    if seen is None:
+        seen = []
+    x, y = current_point
+    seen.append(current_point)
+    print(current_point, 'click color:', click_color, 'target color:', target_color, 'seen:', seen)
+    neighbors = [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]
+
+    index_filter = lambda t: t[0] >= 0 and t[1] >= 0 and t[0] < len(pixels) and t[1] < len(pixels[t[0]])
+    color_filter = lambda t: pixels[t[0]][t[1]] == click_color
+    seen_filter = lambda t: t not in seen
+
+    filtered_neighbors = filter(seen_filter, filter(color_filter, filter(index_filter, neighbors)))
+
+    print('filtered neighbors:', filtered_neighbors)
+    try:
+        current_point_color = pixels[x][y]
+    except IndexError:
+        return
+    if current_point_color == click_color:
+        print('setting', current_point, 'to target color')
+        pixels[x][y] = target_color
+        for coords in filtered_neighbors:
+            print('recursing to:', coords)
+            splash_recurse(pixels, coords, click_color, target_color, seen)
+    else:
+        print('return from the else!')
+
+
+
+
