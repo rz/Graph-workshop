38279b7c60dc52ee1d1e9edcce06f3c94d95a262
Rodrigo Guzman <rodguze@gmail.com>
2016-10-20 10:53:55 -0400

    dynamic programming solution to the min-square-sum problem

--- squares_your_solution.py
+++ squares_your_solution.py
@@ -1,11 +1,32 @@
 # Skeleton code for participant solutions to the squares problem presented
 # in workshop. To run the tests for this problem, run the squares_tests.py script
+import math
 
+def largest_square(n):
+    """Returns the largest perfect square that is smaller than n"""
+    return int(math.floor(math.sqrt(n)) ** 2)
+
+
+cache = {}
 def min_square_sum( n ):
     # Inputs:
     # n: int
 
     # YOUR CODE HERE. Return an integer representing the minimum number of
     # perfect squares required to sum to n.
+    if n <= 0:
+        return None
+    if n in cache:
+        return cache[n]
+    else:
+        lgst_sq = largest_square(n)
+        if lgst_sq == n:
+            return 1
+        else:
+            squares = [i**2 for i in range(1, n) if i**2 <= lgst_sq]
+            # print(squares)
+            l = [min_square_sum(n - s) for s in squares if s < n]
+            cache[n] = 1 + min(l)
+            return cache[n]
 
-    return 0
+
